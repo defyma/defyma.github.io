01f4f438f4c478b76896a9af33605b92628c0f49
Defy M Aminuddin <defyma85@gmail.com>
2022-02-05 15:36:02 +0700

    init-project

---
+++ project/36-bear-doodles/36/bear/doodles/6aa249ef59ca894e16821.js
@@ -0,0 +1,13 @@
+/*!
+ * 
+ * ********************************************************************************
+ *     36 Bear Doodles
+ *     Token Hash: 0x9FA7f20B0F6BE51131a404904754F1d72Ee2d5c6
+ *     Contract: https://polygonscan.com/address/0x9FA7f20B0F6BE51131a404904754F1d72Ee2d5c6#code
+ * 
+ *     Web Build Date: Sat Feb 05 2022 15:30:53 GMT+0700 (Western Indonesia Time)
+ *     Web Version: 1.0.0-1644049853828
+ * ******************************************export default {**************************************
+ *                 
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{259:function(s,t,e){"use strict";e.r(t);var n={components:{},data:function(){return{themeName:"Blue",cssName:""}},head:function(){return{link:[{rel:"stylesheet",href:"".concat(this.env("baseAssets"),"/css/production.css")},{rel:"stylesheet",href:"https://fonts.googleapis.com/css?family=Karla:400,700"}]}},mounted:function(){this.loadJS()},methods:{loadJS:function(){var s=this;this.script("https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js",function(){s.script("".concat(s.env("baseAssets"),"/js/production.js"),function(){})})}}},e=e(97),e=Object(e.a)(n,function(){var s=this.$createElement,s=this._self._c||s;return s("section",{staticClass:"app-main"},[s("nuxt")],1)},[],!1,null,null,null);t.default=e.exports}}]);
---
+++ project/36-bear-doodles/36/bear/doodles/b80a640b02a483210fcc0.js
@@ -0,0 +1,13 @@
+/*!
+ * 
+ * ********************************************************************************
+ *     36 Bear Doodles
+ *     Token Hash: 0x9FA7f20B0F6BE51131a404904754F1d72Ee2d5c6
+ *     Contract: https://polygonscan.com/address/0x9FA7f20B0F6BE51131a404904754F1d72Ee2d5c6#code
+ * 
+ *     Web Build Date: Sat Feb 05 2022 15:30:53 GMT+0700 (Western Indonesia Time)
+ *     Web Version: 1.0.0-1644049853828
+ * ******************************************export default {**************************************
+ *                 
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{129:function(e,s,t){"use strict";var o=t(1),n=t(184),i=t.n(n);o.a.mixin({methods:{script:function(e,s){return i()(e,s)}}})},130:function(e,s,t){"use strict";var o=t(1),n={base:"/project/36-bear-doodles",baseAssets:"/project/36-bear-doodles/assets",baseTheme:"/assets/limitless201-bs4",baseUrl:"https://defyma.github.io",baseApiUrl:"http://localhost/ctmsapi-v15/public",googleMapsKey:"xxxxx",pt:"Defy Ma",appName:"36 Bear Doodles"},i=t(183);o.a.mixin({data:function(){return{}},methods:{env:function(e){return"version"===e?this.version():n[e]},version:function(){return i.version}}})},183:function(e){e.exports=JSON.parse('{"version":"1.0.0"}')},186:function(e,s,t){t(187),e.exports=t(188)}},[[186,4,2]]]);
