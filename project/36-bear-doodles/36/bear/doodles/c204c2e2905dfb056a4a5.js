4dd8b1c6b9be21fa025e66f58fff00d988b61cae
Defy M Aminuddin <defyma85@gmail.com>
2022-02-14 00:08:18 +0700

    Add seen on upcomingnft

--- project/36-bear-doodles/36/bear/doodles/c204c2e2905dfb056a4a5.js
+++ project/36-bear-doodles/36/bear/doodles/c204c2e2905dfb056a4a5.js
@@ -5,8 +5,8 @@
  *     Token Hash: 0xbaF02040337B6DfD745563D259718Da3BC62Fb70
  *     Contract: https://polygonscan.com/address/0xbaF02040337B6DfD745563D259718Da3BC62Fb70#code
  * 
- *     Web Build Date: Sun Feb 13 2022 21:13:05 GMT+0700 (Western Indonesia Time)
- *     Web Version: 1.0.0-1644761585106
+ *     Web Build Date: Mon Feb 14 2022 00:06:52 GMT+0700 (Western Indonesia Time)
+ *     Web Version: 1.0.0-1644772012338
  * ******************************************export default {**************************************
  *                 
  */
