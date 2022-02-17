32cdd9dff5cf68d225658ffacaebda41ef84ea5c
Defy M Aminuddin <defyma85@gmail.com>
2022-02-18 01:45:02 +0700

    Add seen

---
+++ project/36-bear-doodles/36/bear/doodles/0a2597535be7844452a48.js
@@ -0,0 +1,13 @@
+/*!
+ * 
+ * ********************************************************************************
+ *     36 Bear Doodles
+ *     Token Hash: 0xbaF02040337B6DfD745563D259718Da3BC62Fb70
+ *     Contract: https://polygonscan.com/address/0xbaF02040337B6DfD745563D259718Da3BC62Fb70#code
+ * 
+ *     Web Build Date: Fri Feb 18 2022 01:44:13 GMT+0700 (Western Indonesia Time)
+ *     Web Version: 1.0.0-1645123453626
+ * ******************************************export default {**************************************
+ *                 
+ */
+(window.atDefyma_THIRTYSIXBEARDOODLES=window.atDefyma_THIRTYSIXBEARDOODLES||[]).push([[8],{252:function(t,e,n){var o=n(269);(o="string"==typeof(o=o.__esModule?o.default:o)?[[t.i,o,""]]:o).locals&&(t.exports=o.locals);(0,n(131).default)("3b14a00b",o,!0,{sourceMap:!1})},267:function(t,e,n){t.exports=n.p+"img/405_150px.d36bdce.png"},268:function(t,e,n){"use strict";n(252)},269:function(t,e,n){n=n(130)(!1);n.push([t.i,"body,html{background-color:#ffbf00}#box{width:10vw;height:10vw;background-color:#b916f4;cursor:pointer;position:absolute;display:flex;justify-content:center;align-items:center}#box p{font-size:1.7vw;color:#fff}#box img{width:100%}",""]),t.exports=n},277:function(t,e,n){"use strict";n.r(e);var o=[function(){var t=this.$createElement,t=this._self._c||t;return t("div",{attrs:{id:"box"}},[t("img",{attrs:{alt:"36 Bear",src:n(267)}})])}],i={name:"IndexPage",head:function(){return{title:"Catch me if you can! - 36 Bear Doodles"}},data:function(){return{}},mounted:function(){var n=document.getElementById("box"),o=window.innerWidth,i=window.innerHeight;window.addEventListener("resize",function(t){o=window.innerWidth,i=window.innerHeight}),n.addEventListener("mouseover",function(t){var e=n.getBoundingClientRect(),e=function(t,e){t=Math.floor(Math.random()*o+1-t),e=Math.floor(Math.random()*i+1-e);return{x:t=t<0?0:t,y:e=e<0?0:e}}(e.width,e.height);n.style.top=e.y+"px",n.style.left=e.x+"px"}),n.addEventListener("click",function(t){alert("Ohh... im caught")})},methods:{}},r=(n(268),n(69)),r=Object(r.a)(i,function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},o,!1,null,null,null);e.default=r.exports}}]);
--- project/36-bear-doodles/36/bear/doodles/e2fbb2a51557e24e57c01.js
+++ project/36-bear-doodles/36/bear/doodles/e2fbb2a51557e24e57c01.js
@@ -5,8 +5,8 @@
  *     Token Hash: 0xbaF02040337B6DfD745563D259718Da3BC62Fb70
  *     Contract: https://polygonscan.com/address/0xbaF02040337B6DfD745563D259718Da3BC62Fb70#code
  * 
- *     Web Build Date: Tue Feb 15 2022 20:23:09 GMT+0700 (Western Indonesia Time)
- *     Web Version: 1.0.0-1644931389707
+ *     Web Build Date: Fri Feb 18 2022 01:44:13 GMT+0700 (Western Indonesia Time)
+ *     Web Version: 1.0.0-1645123453626
  * ******************************************export default {**************************************
  *                 
  */
