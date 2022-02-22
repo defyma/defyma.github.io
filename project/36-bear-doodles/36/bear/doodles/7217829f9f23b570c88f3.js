14b167b771b7f2636926a590030dea2510c07629
Defy M Aminuddin <defyma85@gmail.com>
2022-02-22 21:58:14 +0700

    Update Game + Embed OS

--- project/36-bear-doodles/36/bear/doodles/7217829f9f23b570c88f3.js
+++ project/36-bear-doodles/36/bear/doodles/7217829f9f23b570c88f3.js
@@ -5,8 +5,8 @@
  *     Token Hash: 0xbaF02040337B6DfD745563D259718Da3BC62Fb70
  *     Contract: https://polygonscan.com/address/0xbaF02040337B6DfD745563D259718Da3BC62Fb70#code
  * 
- *     Web Build Date: Fri Feb 18 2022 01:44:13 GMT+0700 (Western Indonesia Time)
- *     Web Version: 1.0.0-1645123453626
+ *     Web Build Date: Tue Feb 22 2022 21:57:10 GMT+0700 (Western Indonesia Time)
+ *     Web Version: 1.0.0-1645541830033
  * ******************************************export default {**************************************
  *                 
  */
